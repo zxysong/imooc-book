76940806683c03fb9510ec06b90c120882179b44
zxy54587 <1453049128@qq.com>
2020-04-30 19:20:21 +0800

    Enter

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,18 @@
   </div>
 </template>
 
-<style>
-
+<style lang="scss" scoped>
+  @import '@/assets/styles/global';
+  #app {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 0;
+    overflow: hidden;
+    width: 100%;
+    height: 100%;
+    background: $app-bgc;
+  }
 </style>
